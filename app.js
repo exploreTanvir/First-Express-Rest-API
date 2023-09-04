4cf395696c47c04c343f6ea22de5ae1a9bfeb063
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-09-04 10:17:33 +0600

    Undefined Route

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const router=require("./src/routes/api")
 
 app.use("/api/v1",router)
 
+
+// Undefinded Route
+app.use("*",(req,res)=>{
+     res.status(404).json({"Status":"Failed","Data":"Data not found"})
+})
+
 module.exports=app
